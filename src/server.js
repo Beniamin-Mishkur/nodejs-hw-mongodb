52fb25c02e32d9f27069c68bab90c2da7e0c2048
Beniamin Mishkur <bmishkur@gmail.com>
2025-10-23 23:04:35 +0200

    changing greetings text

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ export const setupServer = () => {
 
   app.get('/', (req, res) => {
     res.json({
-      message: 'Hello World! Server is active.',
+      message: 'Wellcome back, User! The server is running smoothly.',
     });
   });
 
